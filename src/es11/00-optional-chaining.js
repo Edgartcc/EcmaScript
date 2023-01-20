81619bc00cc1189ca41ad377e985b6a8f01d9cc4
Edgartcc <edgar.tipanguano@outlook.com>
2023-01-19 20:31:08 -0500

    añadiendo nuevos archivos

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+    gndx: {
+        country: 'EC'
+    },
+    ana: {
+        country: 'CO'
+    }
+}
+//el signo de interrogacion significa que entrara a los datos solo si existen.
+console.log(users?.beebeloper?.country);//el signo de interrogacion significa que entrara a los datso solo si existen.
